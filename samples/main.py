03897fd39bd99cb3f898a0480aee90e9377099e6
Peter O'Leary <pete@timelight.com>
2022-01-13 13:32:39 -0800

    Factor sample code into class

--- samples/main.py
+++ samples/main.py
@@ -6,30 +6,53 @@
 from charm.toolbox.pairinggroup import PairingGroup, GT
 from ABE.ac17 import AC17CPABE
 
+class ABEOperations:
 
-def main():
-    # instantiate a bilinear pairing map
-    pairing_group = PairingGroup('MNT224')
+    def __init__(self) -> None:
+        # instantiate a bilinear pairing map
+        self.__pairing_group = PairingGroup('MNT224')
 
-    # AC17 CP-ABE under DLIN (2-linear)
-    cpabe = AC17CPABE(pairing_group, 2)
+        # AC17 CP-ABE under DLIN (2-linear)
+        self.__cpabe = AC17CPABE(self.__pairing_group, 2)
 
-    # run the set up
-    (pk, msk) = cpabe.setup()
+    def setup(self):
+         # run the set up, pk is public key, msk is master secret key
+        (pk, msk) = self.__cpabe.setup()
+        self.__pk = pk
+        self.__msk = msk
+        # TODO: save private key and master key if flags are present
 
-    # generate a key
-    attr_list = ['ONE', 'TWO', 'THREE']
-    key = cpabe.keygen(pk, msk, attr_list)
+    def keygen(self, attr_list):
+        # key is distributed to content receivers
+        return self.__cpabe.keygen(self.__pk, self.__msk, attr_list)
 
+    # make a random message
+    def msg(self):
+        return self.__pairing_group.random(GT)
+
+    def encrypt(self, policy_str, msg):
+        return self.__cpabe.encrypt(self.__pk, msg, policy_str)
+
+    def decrypt(self, ctxt, key):
+        return self.__cpabe.decrypt(self.__pk, ctxt, key)
+
+
+
+def main(inputfile, outputfile, encrypt, pk_file, msk_file, key_file, debug):
+
+    ops = ABEOperations()
+    ops.setup()
+    key = ops.keygen(['ONE', 'TWO', 'THREE'])
+   
     # choose a random message
-    msg = pairing_group.random(GT)
+    msg = ops.msg()
 
     # generate a ciphertext
-    policy_str = '((ONE and THREE) and (TWO OR FOUR))'
-    ctxt = cpabe.encrypt(pk, msg, policy_str)
+    ctxt = ops.encrypt('((ONE and THREE) and (TWO OR FOUR))', msg)
 
     # decryption
-    rec_msg = cpabe.decrypt(pk, ctxt, key)
+    rec_msg = ops.decrypt(ctxt, key)
+
     if debug:
         if rec_msg == msg:
             print ("Successful decryption.")
@@ -37,6 +60,42 @@ def main():
             print ("Decryption failed.")
 
 
+
+import sys, getopt   
+
 if __name__ == "__main__":
-    debug = False
+
     main()
+    exit()
+
+    # skip all the stuff below for now
+
+    inputfile = ''
+    outputfile = ''
+    # operations are: generate, key, encrypt, decrypt
+    operation = None
+    keyfile = ''
+
+    try:
+        opts, args = getopt.getopt(sys.argv,"hdei:o:k:",["ifile=","ofile=","encrypt=", "keyfile="])
+    except getopt.GetoptError:
+        print('main.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -d <direction> -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-k", "--keyfile"):
+            keyfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-e", "--encrypt"):
+            encrypt = arg
+        elif opt in ("-d", "--debug"):
+            debug = arg
+    print('Input file is '), inputfile
+    print('Output file is '), outputfile
+    main(inputfile, outputfile, op, pk_file, msk_file, key_file, debug)
+
