7987590769e39732dc6ad12d0bc3b6ddb00175aa
Peter O'Leary <pete@timelight.com>
2022-01-14 14:29:12 -0800

    Add padding to sample

--- samples/sample1.py
+++ samples/sample1.py
@@ -1,6 +1,8 @@
+from typing_extensions import ParamSpecKwargs
 from charm.schemes.pkenc.pkenc_cs98 import CS98
 from charm.toolbox.eccurve import prime192v1
 from charm.toolbox.ecgroup import ECGroup
+from charm.toolbox.paddingschemes import PKCS7Padding
 
 from pudb import set_trace; set_trace()
 
@@ -10,6 +12,13 @@ pkenc = CS98(groupObj)
 (pk, sk) = pkenc.keygen()
 
 M = b'Hello World!'
-ciphertext = pkenc.encrypt(pk, M)
+
+pad = PKCS7Padding()
+
+M_pad = pad.encode(bytes(M), 20)
+
+ciphertext = pkenc.encrypt(pk, M_pad)
 
 message = pkenc.decrypt(pk, sk, ciphertext)
+
+print(pad.decode(message))
